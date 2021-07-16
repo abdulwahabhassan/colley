d660161b187cad821eafeb78b6dc5a8cf9d1a841
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-07-16 22:48:29 +0100

    changed theme colors, added drawable state selector for like button

--- app/src/main/java/com/colley/android/adapter/PostsFragmentRecyclerAdapter.kt
+++ app/src/main/java/com/colley/android/adapter/PostsFragmentRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.colley.android.adapter
 
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.content.res.Resources
@@ -7,9 +8,15 @@ import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.View.GONE
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.core.content.res.ResourcesCompat.getColor
+import androidx.core.content.res.ResourcesCompat.getDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.colley.android.R
+import com.colley.android.R.*
 import com.colley.android.databinding.ItemGroupBinding
 import com.colley.android.databinding.ItemPostBinding
 import com.colley.android.databinding.ItemSchoolBinding
@@ -26,6 +33,7 @@ class PostsFragmentRecyclerAdapter() : RecyclerView.Adapter<PostsFragmentRecycle
     }
 
     class PostViewHolder (private val itemBinding : ItemPostBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(post: Post, clickListener: ItemClickedListener) = with(itemBinding) {
 
             nameTextView.text = post.name
@@ -57,11 +65,24 @@ class PostsFragmentRecyclerAdapter() : RecyclerView.Adapter<PostsFragmentRecycle
                 else -> promotionCountTextView.text = "${post.promotions} promotions"
             }
 
-            Glide.with(this.root.context).load(post.image).into(contentImageView)
-            Glide.with(this.root.context).load(post.userPhoto).into(userPhotoImageView)
+            Glide.with(root.context).load(post.image).into(contentImageView)
+            Glide.with(root.context).load(post.userPhoto).into(userPhotoImageView)
 
-            this.root.setOnClickListener {
+            root.setOnClickListener {
                 clickListener.onItemClick(post)
+            }
+            likeLinearLayout.setOnClickListener {
+                Toast.makeText(root.context, "like", Toast.LENGTH_SHORT).show()
+                it.isActivated = when (it.isActivated) {
+                    true -> false
+                    false -> true
+                }
+            }
+            commentLinearLayout.setOnClickListener {
+                Toast.makeText(root.context, "comment", Toast.LENGTH_SHORT).show()
+            }
+            promoteLinearLayout.setOnClickListener {
+                Toast.makeText(root.context, "promote", Toast.LENGTH_SHORT).show()
             }
         }
     }
