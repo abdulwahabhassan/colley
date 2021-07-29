aa8714471dcda52f2665114501ce5b99455cdd48
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-07-29 13:37:30 +0100

    setup firebase local emulator

--- app/src/main/java/com/colley/android/MainActivity.kt
+++ app/src/main/java/com/colley/android/MainActivity.kt
@@ -16,6 +16,12 @@ import com.bumptech.glide.Glide
 import com.colley.android.databinding.ActivityMainBinding
 import com.colley.android.model.DummyData
 import com.google.android.material.imageview.ShapeableImageView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +45,20 @@ class MainActivity : AppCompatActivity() {
         navController = findNavController(R.id.mainNavGraphFragmentContainerView)
         drawerLayout = binding.mainActivityDrawerLayout
 
+        //connect the DrawerLayout to your navigation graph by passing it to AppBarConfiguration
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        //To use firebase emulator in development mode
+        if (BuildConfig.DEBUG) {
+            Firebase.database.useEmulator("10.0.2.2", 9000)
+            Firebase.auth.useEmulator("10.0.2.2", 9099)
+            Firebase.storage.useEmulator("10.0.2.2", 9199)
+        }
+
         //sets the profile photo in the header of the navigation view within the drawerLayout
         val header = binding.mainActivityNavigationView.getHeaderView(0)
         val imageView = header.findViewById<ShapeableImageView>(R.id.profileImageView)
         Glide.with(this).load(DummyData.getListOfPosts()[2].userPhoto).into(imageView)
-
-        //connect the DrawerLayout to your navigation graph by passing it to AppBarConfiguration
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
 
         //connect action bar to navController and provide app bar configuration
@@ -52,6 +67,8 @@ class MainActivity : AppCompatActivity() {
 
         //sets up navigation view for use with navController
         binding.mainActivityNavigationView.setupWithNavController(navController)
+
+
 
     }
     //method called when user tries to navigate up within an activity's hierarchy to a previous screen
