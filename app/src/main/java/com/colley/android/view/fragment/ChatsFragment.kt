aec99fc0cf6541995a270e05cfc4edc157b5f0c9
abdulwahab hassan <abdulwahabhassan645@gmail.com>
2021-10-03 16:22:06 +0100

    refactored business logic for clearing chats

--- app/src/main/java/com/colley/android/view/fragment/ChatsFragment.kt
+++ app/src/main/java/com/colley/android/view/fragment/ChatsFragment.kt
@@ -1,9 +1,11 @@
 package com.colley.android.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ActionMode
@@ -15,6 +17,7 @@ import com.colley.android.R
 import com.colley.android.adapter.ChatsRecyclerAdapter
 import com.colley.android.databinding.FragmentPrivateChatsBinding
 import com.colley.android.model.PrivateChat
+import com.colley.android.model.PrivateMessage
 import com.colley.android.view.dialog.NewMessageBottomSheetDialogFragment
 import com.colley.android.wrapper.WrapContentLinearLayoutManager
 import com.firebase.ui.auth.AuthUI
@@ -30,6 +33,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 
 class ChatsFragment :
@@ -160,60 +164,102 @@ class ChatsFragment :
 
             return when (item?.itemId) {
                 R.id.delete_chat_menu_item -> {
-                    //if only one chat is selected, retrieve chatee's name from database
-                    //display the chatee's name in the dialog message
-                    if (selectedChatsCount == 1) {
-                        dbRef.child("profiles").child(listOfSelectedChats[0])
-                            .child("name").get().addOnSuccessListener { dataSnapshot ->
-                                val name = dataSnapshot.getValue<String>()
-                                //show alert dialog to confirm deletion
-                                AlertDialog.Builder(requireContext())
-                                    .setMessage("Delete chat with $name?")
-                                    .setPositiveButton("Yes") { dialog, which ->
-                                        //create an hashmap of paths to set to null
-                                        //only the current user's chat reference will be deleted
-                                        val childUpdates = hashMapOf<String, Any?>(
-                                            "/user-messages/${currentUser.uid}/" +
-                                                    listOfSelectedChats[0] to null,
-                                            "/user-messages/recent-message/${currentUser.uid}/" +
-                                                    listOfSelectedChats[0] to null
-                                        )
-                                        //update the specified paths defined in the hashMap
-                                        dbRef.updateChildren(childUpdates)
-                                        //dismiss dialog
-                                        dialog.dismiss()
-                                        //finish and close action mode by calling
-                                        //onDestroyActionMode method
-                                        mode?.finish()
-                                    }.setNegativeButton("No") { dialog, which ->
-                                        //dismiss dialog
-                                        dialog.dismiss()
-                                    }.show()
+                    AlertDialog.Builder(requireContext())
+                        .setMessage("Delete ${singularOrPlural(listOfSelectedChats, "this chat", "these chats")}?")
+                        .setPositiveButton("Yes") { dialog, which ->
+                            //for each selected chat with a chatId, get a reference to its location on the
+                            //user's messages path
+                            listOfSelectedChats.forEach { chatId ->
+                                //retrieve all the messages in the location(chat) as a list of dataSnapshots
+                                dbRef.child("user-messages").child(currentUser.uid).child(chatId)
+                                    .get().addOnSuccessListener { dataSnapshot ->
+                                        val listOfMessageIds = arrayListOf<String>()
+                                        //parse each dataSnapshot as PrivateMessage and for each one retrieve
+                                        //its messageId
+                                        dataSnapshot.children.forEach { snapshot ->
+                                            val messageId = snapshot.getValue(PrivateMessage::class.java)?.messageId
+                                            //add messageId to list
+                                            if (messageId != null) {
+                                                listOfMessageIds.add(messageId)
+                                            }
+                                        }
+                                        //if list is not empty meaning that there are messages in the chats
+                                        //perform the following operation on each message
+                                        if (listOfMessageIds.isNotEmpty()) {
+                                            listOfMessageIds.forEach { messageId ->
+                                                dbRef.child("user-messages").child(currentUser.uid)
+                                                    .child(chatId).child(messageId).get()
+                                                    .addOnSuccessListener { dataSnapshot ->
+                                                        val message = dataSnapshot.getValue<PrivateMessage>()
+                                                        //if this message has an image and was uploaded by the
+                                                        //current user, delete and remove from storage
+                                                        if (message?.image != null && message.fromUserId == currentUser.uid) {
+                                                            //get a reference to it's location on database storage from its url and
+                                                            //delete it with the retrieved reference
+                                                            Firebase.storage.getReferenceFromUrl(message.image!!).delete()
+                                                                .addOnCompleteListener { task ->
+                                                                    if (task.isSuccessful) {
+                                                                        //create an hashmap of paths to set to null
+                                                                        //delete the current user's message reference and that of the chatee
+                                                                        //(we only do this when a message has an image to avoid storage in cases
+                                                                        //where both users might delete their references to this message and leave
+                                                                        //the image remaining in storage forever)
+                                                                        val childUpdates = hashMapOf<String, Any?>(
+                                                                            "/user-messages/${currentUser.uid}/${chatId}/" +
+                                                                                    messageId to null,
+                                                                            "/user-messages/${chatId}/${currentUser.uid}/" +
+                                                                                    messageId to null
+                                                                        )
+                                                                        //update the specified paths defined in the hashMap
+                                                                        dbRef.updateChildren(childUpdates)
+                                                                        Toast.makeText(
+                                                                            requireContext(),
+                                                                            "Deleted media and message from both ends",
+                                                                            Toast.LENGTH_LONG).show()
+                                                                    } else {
+                                                                        Toast.makeText(
+                                                                            requireContext(),
+                                                                            "Failed to delete media, hence message from both ends",
+                                                                            Toast.LENGTH_LONG).show()
+                                                                    }
+                                                                }
+                                                        } else {
+                                                            //else if message doesn't contain an image or wasn't uploaded by the user
+                                                            //simply delete their only their own reference of the message without affecting
+                                                            //the chatee's own reference and do not delete media from storage
+                                                            //create an hashmap of paths to set to null
+                                                            //only the current user's message reference will be deleted
+                                                            val childUpdates = hashMapOf<String, Any?>(
+                                                                "/user-messages/${currentUser.uid}/${chatId}/" +
+                                                                        messageId to null
+                                                            )
+                                                            //update the specified paths defined in the hashMap
+                                                            dbRef.updateChildren(childUpdates)
+                                                        }
+                                                    }
+                                            }
+                                            //Toast that chat has been deleted after all messages has bee deleted
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "Deleted chat",
+                                                Toast.LENGTH_LONG).show()
+                                        } else {
+                                            //Toast that chat is empty
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "Empty chat",
+                                                Toast.LENGTH_LONG).show()
+                                        }
+                                    }
                             }
-                    } else {
-                        AlertDialog.Builder(requireContext())
-                            .setMessage("Delete $selectedChatsCount chats?")
-                            .setPositiveButton("Yes") { dialog, which ->
-                                listOfSelectedChats.forEach {
-                                    //create an hashmap of paths to set to null
-                                    //only the current user's chat reference will be deleted
-                                    val childUpdates = hashMapOf<String, Any?>(
-                                        "/user-messages/${currentUser.uid}/$it" to null,
-                                        "/user-messages/recent-message/${currentUser.uid}/" +
-                                                it to null
-                                    )
-                                    //update the specified paths defined in the hashMap
-                                    dbRef.updateChildren(childUpdates)
-                                }
-                                //dismiss dialog
-                                dialog.dismiss()
-                                //finish and close action mode by calling onDestroyActionMode method
-                                mode?.finish()
-                            }.setNegativeButton("No") { dialog, which ->
-                                //dismiss dialog
-                                dialog.dismiss()
-                            }.show()
-                    }
+                            //dismiss dialog
+                            dialog.dismiss()
+                            //finish and close action mode by calling onDestroyActionMode method
+                            mode?.finish()
+                        }.setNegativeButton("No") { dialog, which ->
+                            //dismiss dialog
+                            dialog.dismiss()
+                        }.show()
                     true
                 } else -> false
             }
@@ -230,6 +276,20 @@ class ChatsFragment :
             adapter?.restBackgroundOfSelectedViews()
             actionMode = null
         }
+    }
+
+
+    private fun singularOrPlural(list: ArrayList<String>, singular: String, plural: String): String {
+        val size = list.size
+        var output = ""
+        if (list.isNotEmpty()) {
+            output = if (size == 1) {
+                singular
+            } else {
+                plural
+            }
+        }
+        return output
     }
 
     //this method is used to keep track of selected items and update the visual state of views
